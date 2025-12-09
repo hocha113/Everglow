8068e8bed4e69637a09c710ffba4686f4494010a
Cloudea <cloudeasoft@qq.com>
2025-12-09 14:02:36 +0800

    refactor: Add code regions to MissionManager

--- Sources/Everglow.Function/Mechanics/MissionSystem/Core/MissionManager.cs
+++ Sources/Everglow.Function/Mechanics/MissionSystem/Core/MissionManager.cs
@@ -35,17 +35,15 @@ public static class MissionManager
 	/// </summary>
 	public static bool NeedRefresh { get; set; } = false;
 
+	#region TML Integration
+
 	public static void Load()
 	{
 		if (!Main.dedServ)
 		{
-			_missionPools = [];
+			_missionPools = Enum.GetValues<PoolType>()
+				.ToDictionary(t => t, _ => new List<MissionBase>());
 			_nPCKillCounter = [];
-
-			foreach (var missionPoolType in Enum.GetValues<PoolType>())
-			{
-				_missionPools.Add(missionPoolType, []);
-			}
 
 			Main.OnTickForInternalCodeOnly += Update;
 			MissionPlayer.OnKillNPCEvent += MissionPlayer_OnKillNPC_CountKill;
@@ -62,13 +60,121 @@ public static class MissionManager
 		}
 	}
 
-	public static void ClearAllEvents()
+	/// <summary>
+	/// Initialize mission manager with player mission data
+	/// </summary>
+	/// <param name="data"></param>
+	public static void ApplyData(MissionManagerData data)
 	{
+		if (data == null)
+		{
+			return;
+		}
+
+		_nPCKillCounter = data.NPCKillCounter.ToDictionary();
+		_missionPools = data.MissionPools.ToDictionary();
+
+		AcceptedMissionPool.ForEach(x => x.Activate());
+	}
+
+	/// <summary>
+	/// 清除所有任务池中的任务
+	/// </summary>
+	public static void Clear()
+	{
+		// Clear all events.
 		foreach (var m in _missionPools.SelectMany(x => x.Value))
 		{
 			m.Deactivate();
 		}
+
+		_nPCKillCounter.Clear();
+		foreach (var (_, missionPool) in _missionPools)
+		{
+			missionPool.Clear();
+		}
 	}
+
+	#endregion
+
+	#region In-Game Update
+
+	/// <summary>
+	/// 为任务每帧更新
+	/// </summary>
+	public static void Update()
+	{
+		// Main.gamePaused always be false here when triggered by Main.OnTickForInternalCodeOnly hook.
+		if (Main.gameMenu || Main.gameInactive) // || Main.gamePaused
+		{
+			return;
+		}
+
+		if (Main.timeForVisualEffects % UpdateInterval != 0)
+		{
+			return;
+		}
+
+		// 更新所有任务
+		AcceptedMissionPool.ForEach(m => m.Update());
+
+		// 处理自动提交任务
+		var autoCommitMissions = AcceptedMissionPool.Where(m => m.CheckComplete() && m.AutoComplete).ToList();
+		if (autoCommitMissions.Count > 0)
+		{
+			autoCommitMissions.ForEach(m => m.OnComplete());
+			NeedRefresh = true;
+		}
+
+		// 处理过期任务
+		var expiredMissions = AcceptedMissionPool.Where(m => m.CheckExpire()).ToList();
+		if (expiredMissions.Count > 0)
+		{
+			expiredMissions.ForEach(m => m.OnExpire());
+			NeedRefresh = true;
+		}
+
+		// 检测可提交状态改变的任务，将状态改变为可提交的任务抛出信息
+		foreach (var m in AcceptedMissionPool.ToList())
+		{
+			if (m.CheckComplete() != m.OldCheckComplete)
+			{
+				m.OldCheckComplete = m.CheckComplete();
+
+				m.OnCheckCompleteChange();
+
+				// 由不可提交改变到可提交状态的任务, 发送消息提示
+				if (m.CheckComplete())
+				{
+					Main.NewText($"[{m.Name}]任务可以提交了", 250, 250, 150);
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// 记录杀怪
+	/// </summary>
+	/// <param name="npc">被击杀的NPC</param>
+	/// <exception cref="InvalidParameterException">参数为空或npc类型错误</exception>
+	public static void MissionPlayer_OnKillNPC_CountKill(NPC npc)
+	{
+		if (npc.type <= NPCID.None)
+		{
+			Ins.Logger.Warn($"Invalid npc type {npc.type}.");
+			return;
+		}
+
+		// Update NPC kill history
+		if (!_nPCKillCounter.TryAdd(npc.type, 1))
+		{
+			_nPCKillCounter[npc.type]++;
+		}
+	}
+
+	#endregion
+
+	#region Manager Logic
 
 	/// <summary>
 	/// 获取某个类型的所有任务
@@ -283,110 +389,9 @@ public static class MissionManager
 	/// <returns></returns>
 	public static List<MissionBase> GetMissionPool(PoolType type) => _missionPools[type][..];
 
-	/// <summary>
-	/// 为任务每帧更新
-	/// </summary>
-	public static void Update()
-	{
-		if (Main.gameMenu || Main.gamePaused || Main.gameInactive)
-		{
-			return;
-		}
-
-		if (Main.timeForVisualEffects % UpdateInterval != 0)
-		{
-			return;
-		}
-
-		// 更新所有任务
-		AcceptedMissionPool.ForEach(m => m.Update());
-
-		// 处理自动提交任务
-		var autoCommitMissions = AcceptedMissionPool.Where(m => m.CheckComplete() && m.AutoComplete).ToList();
-		if (autoCommitMissions.Count > 0)
-		{
-			autoCommitMissions.ForEach(m => m.OnComplete());
-			NeedRefresh = true;
-		}
-
-		// 处理过期任务
-		var expiredMissions = AcceptedMissionPool.Where(m => m.CheckExpire()).ToList();
-		if (expiredMissions.Count > 0)
-		{
-			expiredMissions.ForEach(m => m.OnExpire());
-			NeedRefresh = true;
-		}
-
-		// 检测可提交状态改变的任务，将状态改变为可提交的任务抛出信息
-		foreach (var m in AcceptedMissionPool.ToList())
-		{
-			if (m.CheckComplete() != m.OldCheckComplete)
-			{
-				m.OldCheckComplete = m.CheckComplete();
-
-				m.OnCheckCompleteChange();
-
-				// 由不可提交改变到可提交状态的任务, 发送消息提示
-				if (m.CheckComplete())
-				{
-					Main.NewText($"[{m.Name}]任务可以提交了", 250, 250, 150);
-				}
-			}
-		}
-	}
-
-	/// <summary>
-	/// 清除所有任务池中的任务
-	/// </summary>
-	public static void Clear()
-	{
-		ClearAllEvents();
-		_nPCKillCounter.Clear();
-		foreach (var (_, missionPool) in _missionPools)
-		{
-			missionPool.Clear();
-		}
-	}
-
-	/// <summary>
-	/// 记录杀怪
-	/// </summary>
-	/// <param name="npc">被击杀的NPC</param>
-	/// <exception cref="InvalidParameterException">参数为空或npc类型错误</exception>
-	public static void MissionPlayer_OnKillNPC_CountKill(NPC npc)
-	{
-		if (npc is null || npc.type < NPCID.None)
-		{
-			throw new InvalidParameterException();
-		}
-
-		// Update NPC kill history
-		if (!_nPCKillCounter.TryAdd(npc.type, 1))
-		{
-			_nPCKillCounter[npc.type]++;
-		}
-	}
+	#endregion
 
 	#region Data Persistance
-
-	/// <summary>
-	/// Initialize mission manager with player mission data
-	/// </summary>
-	/// <param name="data"></param>
-	public static void ApplyData(MissionManagerData data)
-	{
-		if (data == null)
-		{
-			return;
-		}
-
-		Clear();
-
-		_nPCKillCounter = data.NPCKillCounter.ToDictionary();
-		_missionPools = data.MissionPools.ToDictionary();
-
-		AcceptedMissionPool.ForEach(x => x.Activate());
-	}
 
 	/// <summary>
 	/// Save missions to player file data.
